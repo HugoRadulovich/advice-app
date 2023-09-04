3fe518e08a4e9baa408036aaf443d8845b53d5d4
Hugo Radulovich <hcradulovich@gmail.com>
2023-09-04 20:34:15 -0300

    fix de erroes

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <main className='h-screen flex flex-col items-center justify-center'>
-       <section className="bg-dark-grayish-blue h-fit-content   w-3/5 flex justify-center items-center flex-col text-center  rounded-3xl gap-9">
+       <section className="bg-dark-grayish-blue h-fit-content w-3/5 flex justify-center items-center flex-col text-center  rounded-3xl gap-9">
           {
             idAdvice && <h1 className="text-neon-green text-sm font-manrope tracking-[.40em] text-center p-4">ADVICE #{idAdvice}</h1>
           }
@@ -39,7 +39,7 @@ function App() {
             advice &&  <p className="text-light-cyan text-3xl text-center  font-manrope line-clamp-3  font-semibold leading-relaxed ">"{advice}"</p>
           }
           <img src="src\assets\pattern-divider-desktop.svg" alt="pattern divider for desktop" className="" />
-         <div className=" relative top-9">
+         <div className=" relative top-8">
          <button className="bg-neon-green w-16 h-16 rounded-full flex justify-center items-center shadow-nuevo hover:shadow-neon-green" onClick={handleClick}>
             <img src='src\assets\icon-dice.svg' alt="image of button" className="" />
           </button>
